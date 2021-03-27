1c8a4532e052bb7abc8611091b143d0797f6812c
Eric Mathias <emathias@gmail.com>
2021-03-27 14:02:22 -0400

    socials added to footer

--- src/components/pages/Footer/Footer.js
+++ src/components/pages/Footer/Footer.js
@@ -7,6 +7,25 @@ function Footer() {
             <section className="footer">
                 <p className="footer-heading">
                     Made with React, Node.js, HTML, CSS. All rights reserved. Eric Mathias 2021.
+                    <div className="hero-btns">
+                    <div className="socialbtns">
+                        <button className='social' >
+                            <a href ="https://drive.google.com/file/d/1FOC_Ug4Xd9TI5TEqZ8K-kIoAPP2bPQOW/view?usp=sharing">
+                                <img src="/images/resumeIcon.png" className="icons" alt=""></img>
+                            </a>
+                        </button>
+                        <button className='social'>
+                            <a href ="https://www.linkedin.com/in/ericgmathias/">
+                                <img src="/images/linkedin.png" className="icons" alt=""></img>
+                            </a>
+                        </button>
+                        <button className='social'>
+                            <a href ="https://github.com/calmlb">
+                                <img src="/images/GitHub-Mark.png" alt=""></img> 
+                            </a>
+                        </button>
+                    </div>
+            </div>
                 </p> 
             </section>
             <div className="footer-links">
